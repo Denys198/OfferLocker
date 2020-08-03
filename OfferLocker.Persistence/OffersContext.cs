aab8d9fc6b022308eb5e9e16ac1fd6ca77ff8f3a
tc-yonutz <ciprian-ionut.tanasa@student.tuiasi.ro>
2020-08-03 16:05:26 +0300

    Adaugare functionalitate pentru tablele University, Faculty, Student, CampusCommunity si UserType

---
+++ OfferLocker.Persistence/OffersContext.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OfferLocker.Entities.Offers;
+using OfferLocker.Entities.Identity;
+using OfferLocker.Entities.Commons;
+
+namespace OfferLocker.Persistence
+{
+	public sealed class OffersContext : DbContext
+	{
+        public OffersContext(DbContextOptions<OffersContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Offer> Offers { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<University> Universities { get; set; }
+        public DbSet<Faculty> Faculties { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<CampusCommunity> CampusCommunities { get; set; }
+        public DbSet<UserType> UserTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Offer>()
+                .HasMany<Comment>(offer => offer.Comments)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Offer>()
+                .HasMany<Photo>(offer => offer.Photos)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Comment>()
+                .Property(c => c.Id)
+                .IsRequired()
+                .ValueGeneratedNever();
+
+            modelBuilder.Entity<Photo>()
+                .Property(c => c.Id)
+                .IsRequired()
+                .ValueGeneratedNever();
+
+            modelBuilder.Entity<User>()
+                .HasIndex(x => x.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<University>()
+                .HasMany<Faculty>(p => p.Faculties)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Faculty>()
+                .HasMany<CampusCommunity>(p => p.CampusCommunities)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+            modelBuilder.Entity<Faculty>()
+                .HasMany<Student>(p => p.Students)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Student>()
+                .Property(p => p.Id)
+                .IsRequired()
+                .ValueGeneratedNever();
+
+            modelBuilder.Entity<CampusCommunity>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<UserType>()
+                .HasMany(p => p.Users)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
